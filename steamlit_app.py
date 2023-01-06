c6bda2c6616d28773c7f146fd4becf934033c932
DannT <87823905+DannTL@users.noreply.github.com>
2023-01-06 13:18:57 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list = fruit_list.set_index('Fruit')
 
 
 st.title('My Parents New Healthy Diner')
