43c337401cef7444e29c93c1aacae72b8fa3faa5
DannT <87823905+DannTL@users.noreply.github.com>
2023-01-09 09:17:27 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -21,8 +21,8 @@ fruits_to_show = fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
 
 st.text(fruityvice_response.json())
 
